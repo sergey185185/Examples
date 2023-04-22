d1511137069185f978d45b5fdfc6525cc2e5be5a
sergey185185 <sergey.bychkov.1@mail.ru>
2023-04-22 15:32:18 +0700

    Z003

---
+++ Z003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+ {
+    Console.WriteLine("Число четное");
+ }
+else 
+ {
+    Console.WriteLine("Число не четное");
+ }
+
