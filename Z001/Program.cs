669eafb27fef8a3c4378dc642b44fe798f652a71
sergey185185 <sergey.bychkov.1@mail.ru>
2023-04-22 14:44:23 +0700

    Z001

---
+++ Z001/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+     Console.WriteLine("{0} наибольшее число", a);
+}        
+else
+{
+     Console.WriteLine("{0} наибольшее число", b);
+}
+
+Console.ReadLine();
